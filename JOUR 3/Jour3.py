7b4c30810578077e918db5153c9d0eeca82e4a87
Antogone <64025310+Antogone@users.noreply.github.com>
2022-12-03 20:55:53 +0100

    Jour 3 - Puzzle 2 en cours

--- JOUR 3/Jour3.py
+++ JOUR 3/Jour3.py
@@ -34,10 +34,29 @@ print("SUM ITEM",valeur)
 with open("input2.txt", "r") as variable:
   contenu2 = variable.readlines()
 
-alpha = []
+beta = []
+commu1=[]
+commu2=[]
 for i in range(0,len(contenu2),3):
-  print(i)
   p1 = contenu[i].replace("\n","")
   p2 = contenu[i+1].replace("\n","")
   p3 = contenu[i+2].replace("\n","")
-  print(p1," ",p2," ",p3)
+  p4 = p2 + "-"+ p3
+  print(p4, p1)
+
+  for j in range(len(p1)):
+    if (p2.find(p1[j]) != -1):
+      commu1.append(p1[j])
+      print(p4.count(p1[j]))
+      # print(badge)
+      # beta.append(badge)
+
+
+valeur = 0
+for i in beta:
+  if (i.islower()):
+    valeur = valeur + ord(i) - 96
+  else:
+    valeur = valeur + (ord(i) + 32 + 26) - 96
+print("SUM ITEM Badge", valeur)
+
