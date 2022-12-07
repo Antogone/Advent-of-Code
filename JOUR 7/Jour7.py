d3c872f635abdb004791dbaa9e87d982728bd7ff
Antogone <64025310+Antogone@users.noreply.github.com>
2022-12-07 15:53:57 +0100

    Jour 7 - Puzzle1 pas terminé

--- JOUR 7/Jour7.py
+++ JOUR 7/Jour7.py
@@ -4,16 +4,37 @@ os.chdir("/Users/anto/Desktop/Advent of Code/Advent-of-Code/JOUR 7")
 data = []
 with open("input.txt", "r") as variable:
   for ligne in variable:
-    words = ligne.rstrip().split(" ")
+    words = ligne.rstrip().split(' ')
     data.append(words)
 
 
-def Puzzle1(Data):
-  for command in Data:
-    # if command[0] =="$":
-    #   if command[1] =="cd":
-    #     print("a")
-    print(command)
+
+for line in data:
+    if line[0] == "$":
+        if line[1] =="cd":
+            curr_dir = line[-1]
+            listing = 0
+        if line[1] == "ls":
+            listing = 1
+    else:
+        print(curr_dir)
+        if line[0] != "dir":
+            print(line)
+        else:
+            print(line)
 
 
-Puzzle1(data)
+# pseudo_tree = {}
+# current_dir = '/'
+# for line in data:
+#     tokens = line.split(' ')
+#     if tokens[0] == "$":
+#       command = tokens[1]
+#       args = tokens[2:]
+#       if command ==
+#       pseudo_tree.update({'command': command, 'taille': None, 'name': args, 'type' : "command"})
+#     else :
+#       args = tokens[0:]
+#       if
+#       pseudo_tree.update({'command': command, 'taille': args[0], 'name': args[1], 'type' : "fichier"})
+#     print(pseudo_tree)
